4fb4f610217d08afc4666d61c497ec5584d3fec5
fidofear <firdah.yuliana@gmail.com>
2023-08-25 00:13:54 +0800

    cek sast php using phpstan 2

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '08ebfe9aefc1aa21ebef0307dde99dddc30d3d31',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '08ebfe9aefc1aa21ebef0307dde99dddc30d3d31',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
